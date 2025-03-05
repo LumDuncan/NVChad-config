acfab8c22d5150e6e4d44c9625cbbfd60dcdf90b
LumDuncan <liam.duncan@conmet.com>
2025-03-05 15:27:29 -0800

    fix keymaps for cmp

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -20,17 +20,27 @@ local plugins = {
         end,
       },
     },
-    opts = {
-      sources = {
+    config = function(_, opts)
+      vim.opt.completeopt = {'menu','menuone','noselect','noinsert'}
+      local cmp = require "cmp"
+      opts.sources = {
         { name = "nvim_lsp", group_index = 2 },
         { name = "copilot",  group_index = 2 },
         { name = "luasnip",  group_index = 2 },
         { name = "buffer",   group_index = 2 },
         { name = "nvim_lua", group_index = 2 },
         { name = "path",     group_index = 2 },
-      },
-    },
-  },
+      }
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<S-CR>"] = cmp.mapping.confirm({ select = true }),
+      })
+      cmp.setup(opts)
+    end,
+   },
 }
 
 return plugins
