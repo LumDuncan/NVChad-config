2f50608cb949e10991627c761d359d2246492690
LumDuncan <liam.duncan@conmet.com>
2025-06-05 10:31:44 -0700

    add witch, dashboard

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,30 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
----@type ChadrcConfig
+-- -@type ChadrcConfig
 local M = {}
+M.plugins = {}
 
 
 M.base46 = {
-	theme = "rxyhn",
+  -- theme = "rxyhn",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.nvdash = {
+  load_on_startup = true,
+}
+
+M.plugins = {
+  status = {
+    dashboard = true,
+    colorizer = true,
+    telescope_media = true,
+  },
 }
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,100 +1,90 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
-  },
+    {
+        "stevearc/conform.nvim",
+        -- event = 'BufWritePre', -- uncomment for format on save
+        config = function()
+            require "configs.conform"
+        end,
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
-  {
-    'numToStr/Comment.nvim',
-    opts = {
-      config = function()
-        require('Comment').setup()
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require "configs.lspconfig"
+        end,
+    },
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            config = function()
+                require('Comment').setup()
 
-        local ft = require('Comment.ft')
+                local ft = require('Comment.ft')
 
-        ft.set('c', '//%s')
-      end
-    }
-  },
-  { 'nvchad/volt', lazy = true },
-  { 'nvchad/menu', lazy = true },
-  {
-    "j-hui/fidget.nvim",
-    config = function()
-      require('fidget').setup()
-    end
+                ft.set('c', '//%s')
+            end
+        }
+    },
+    { 'nvchad/volt', lazy = true },
+    { 'nvchad/menu', lazy = true },
+    {
+        "j-hui/fidget.nvim",
+        config = function()
+            require('fidget').setup()
+        end
 
-  },
-  {
-    "sindrets/diffview.nvim",
-    cmd = "DiffviewOpen",
-    config = function()
-      require("diffview").setup()
-    end,
-  },
-  -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  --   ft = { "markdown" },
-  --   build = function() vim.fn["mkdp#util#install"]() end,
-  -- },
-  -- install with yarn or npm
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  {
-    "dgox16/oldworld.nvim",
-    lazy = false, -- Only load when the plugin is actually in use
-    priority = 1000,
-  },
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^5",
-    ft = { "rust" },
-    config = function(_, _)
-      local on_attach = require("nvchad.configs.lspconfig").on_attach
-      vim.g.rustaceanvim = {
-        server = {
-          on_attach = on_attach },
-      }
-    end
-  },
+    },
+    {
+        "sindrets/diffview.nvim",
+        cmd = "DiffviewOpen",
+        config = function()
+            require("diffview").setup()
+        end,
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+    {
+        "sontungexpt/witch",
+        priority = 1000,
+        lazy = false,
+        config = function(_, opts)
+            require("witch").setup {
+                theme = {
+                    enabled = true,
+                    style = "dark",
+                    extras = {
+                        bracket = true,
+                        dashboard = true,
+                        diffview = true,
+                        explorer = true,
+                    },
+                },
+                dim_inactive = {
+                    enabled = true,
+                    level = 0.70,
+                }
+            }
+        end,
+    },
+    {
+        "mrcjkb/rustaceanvim",
+        version = "^5",
+        ft = { "rust" },
+        config = function(_, _)
+            local on_attach = require("nvchad.configs.lspconfig").on_attach
+            vim.g.rustaceanvim = {
+                server = {
+                    on_attach = on_attach },
+            }
+        end
+    },
 }
